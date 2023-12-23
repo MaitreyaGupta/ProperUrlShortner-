5dee94bd43d3e499f1697165bf090f3d8fd90ae0
Maitreya Gupta <95370078+MaitreyaGupta@users.noreply.github.com>
2023-12-23 12:01:19 +0530

    Create URLShortner.js

---
+++ models/URLShortner.js
@@ -0,0 +1,25 @@
+const mongoose=require("mongoose")
+const MongoDb=require("../config/mongoose1")
+
+const Item=new mongoose.Schema({
+    username:
+    [
+        type=String,
+        require=true,
+        unique=true 
+    ],
+    password:
+    [
+        type=String,
+        require=true,
+        unique=true
+    ],
+    recoveryPhoneNo:[
+        type=String,
+        rquire=true,
+    ]
+})
+
+const ItemMode=mongoose.model("Item",Item)
+
+module.exports=ItemMode;
